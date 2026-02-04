be51fb027fd23183299eccde98616f18c0ccde1d
imammv <imammuslim060892@gmail.com>
2026-02-04 20:47:29 +0700

    v0.0.13

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it and Awesome V0.0.12" />
+      <HelloWorld msg="You did it and Awesome V0.0.13" />
     </div>
   </header>
 
